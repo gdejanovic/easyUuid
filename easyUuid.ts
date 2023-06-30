eab176c69683c48a52a62589b03b4ff14f194307
gdejanovic <gorandejanovic@gmail.com>
2023-06-30 23:04:58 +0200

    formatted code

--- easyUuid.ts
+++ easyUuid.ts
@@ -1,14 +1,13 @@
 class EasyUuid {
     private static counter: number = 0;
-  
+
     public static generate(): string {
-      const timestamp: string = Date.now().toString(16);
-      const random: string = Math.floor(Math.random() * 1000000000).toString(16);
-      const count: string = (EasyUuid.counter++).toString(16);
-  
-      return `${random}-${random}-${count}-${random}-${timestamp}`;
+        const timestamp: string = Date.now().toString(16);
+        const random: string = Math.floor(Math.random() * 1000000000).toString(16);
+        const count: string = (EasyUuid.counter++).toString(16);
+
+        return `${random}-${random}-${count}-${random}-${timestamp}`;
     }
-  }
-  
-  export default EasyUuid;
-  
+}
+
+export default EasyUuid;
